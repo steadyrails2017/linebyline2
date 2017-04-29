c34b387f4121dc354c1095887698e5da128f2822
JD <thedailydeveloper@gmail.com>
2017-04-29 01:08:51 -0400

    RSpec Tests are going as planned and it's now time to install the Simple_Form Gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root  'displays#index'
+
+  resources :displays
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/controllers/displays_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DisplaysController, type: :controller do
+
+end
